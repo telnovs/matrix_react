8ff81bcc581eed91387ce656ad5d65c9cf061521
Aleksey Sokolov <alexis.sokolov@yahoo.com>
2020-09-28 11:41:27 +0300

    С таймером закончили...

--- src/Components/MatrixList.jsx
+++ src/Components/MatrixList.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { getMatrices, getLastSuccessBaking } from "../utils/backend";
-import { matrixFullName, secondsToTime } from "../utils/helpers";
+import { matrixFullName } from "../utils/helpers";
 import Timer from "./Timer";
 
 const MatrixList = () => {
-   const [timerMinutes, setTimerMinutes] = useState(0);
-   const [timerSeconds, setTimerSeconds] = useState(0);
-
    const [matrixList, setMatrixList] = useState([]);
 
-   const [startFlag, setStartFlag] = useState(false);
+   const [timerSeconds, setTimerSeconds] = useState(0);
 
    const listOfMatricesArrived = (matricesFromBackend) => {
       setMatrixList(matricesFromBackend);
@@ -19,25 +16,15 @@ const MatrixList = () => {
       getMatrices(listOfMatricesArrived);
    }, []);
 
-   const minutesOnChange = (event) => setTimerMinutes(parseInt(event.target.value, 10));
-
-   const secondsOnChange = (event) => setTimerSeconds(parseInt(event.target.value, 10));
-
    const lastSuccessBakingArrived = (lastSuccessBaking) => {
-      if (lastSuccessBaking.length === 0) {
-         return;
+      if (lastSuccessBaking.length > 0) {
+         setTimerSeconds(lastSuccessBaking[0].value[0].durationInSeconds);
       }
-
-      const tempTime = secondsToTime(lastSuccessBaking[0].value[0].durationInSeconds);
-      setTimerMinutes(tempTime.minutes);
-      setTimerSeconds(tempTime.seconds);
    };
 
    const selectOnChange = (event) => {
       getLastSuccessBaking(event.target.value, lastSuccessBakingArrived);
    };
-
-   const startButtonOnClick = () => setStartFlag(true);
 
    return (
       <div>
@@ -60,42 +47,8 @@ const MatrixList = () => {
                      );
                   })}
                </select>
-               <div className="input-group mb-3">
-                  <input
-                     type="number"
-                     className="form-control"
-                     placeholder="мин"
-                     onChange={(e) => {
-                        minutesOnChange(e);
-                     }}
-                     value={timerMinutes}
-                  />
-                  <div className="input-group-append">
-                     <span className="input-group-text" id="basic-addon2">
-                        :
-                     </span>
-                  </div>
-                  <input
-                     type="number"
-                     className="form-control"
-                     placeholder="сек"
-                     onChange={(e) => {
-                        secondsOnChange(e);
-                     }}
-                     value={timerSeconds}
-                  />
-                  <button
-                     type="button"
-                     className="btn btn-danger"
-                     onClick={() => {
-                        startButtonOnClick();
-                     }}
-                  >
-                     Запуск!
-                  </button>
-               </div>
-               <Timer timerSeconds={timerMinutes * 60 + timerSeconds} startFlag={startFlag}/>
             </div>
+            <Timer timerSeconds={timerSeconds} />
          </form>
       </div>
    );
--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,28 +1,113 @@
-import React, { useEffect, useState } from "react";
-import usePreciseTimer from "./usePreciseTimer";
+import React, { useEffect, useState, useRef } from "react";
 import { secondsToTime } from "../utils/helpers";
 
 const Timer = (props) => {
-   let { startFlag } = props;
+   const [timer, setTimer] = useState(0);
+   const [startFlag, setStartFlag] = useState(false);
    const { timerSeconds } = props;
 
-   const [timer, setTimer] = useState(0);
+   const timerTikTak = useRef(null);
+   const intervalObject = useRef(null);
+   const startTime = useRef(null);
 
-   const timerTikTak = (delay) => {
-      if (timer > 0) {
-         setTimer(timer - delay);
-         console.log("тик-так... ", delay);
+   const addNewBaking = () => {
+      console.log("Новая выпечка!!");
+   }
+
+   timerTikTak.current = (delay) => {
+      const tempTimer = timer - delay;
+
+      if (tempTimer <= 0) {
+         setStartFlag(false);
+         clearInterval(intervalObject.current);
+         addNewBaking();
       }
-      startFlag = false;
+      setTimer(tempTimer);
+      console.log("тик-так... ", delay);
+
+      console.log("таймер: ", tempTimer);
    };
 
    useEffect(() => {
+      console.log("ставим время таймера: ", timerSeconds);
       setTimer(timerSeconds);
-   }, [timerSeconds, startFlag]);
+   }, [timerSeconds]);
 
-   usePreciseTimer(timerTikTak, 1000, startFlag);
+   // eslint-disable-next-line consistent-return
+   useEffect(() => {
+      console.log("хук таймера...");
+      if (startFlag) {
+         console.log("Запуск займера...");
+         startTime.current = Date.now();
+         intervalObject.current = setInterval(() => {
+            const currentTime = Date.now();
+            const delay = currentTime - startTime.current;
+            startTime.current = currentTime;
+            timerTikTak.current(delay / 1000);
+         }, 1000);
 
-   return <div className="display-1">{`${secondsToTime(Math.ceil(timer)).string}`}</div>;
+         return () => {
+            clearInterval(intervalObject.current);            
+         }
+      }
+   }, [startFlag]);
+
+   return (
+      <div className="container">
+         <div className="row justify-content-between">
+            <div className="col-6 text-left">
+               <button
+                  type="button"
+                  className="btn btn-secondary btn-sm"
+                  onClick={() => {
+                     setTimer(timer + 60);
+                  }}
+               >
+                  +1
+               </button>
+               <button
+                  type="button"
+                  className="btn btn-secondary btn-sm"
+                  onClick={() => {
+                     setTimer(timer - 60);
+                  }}
+               >
+                  -1
+               </button>
+            </div>
+            <div className="col-6 text-right">
+               <button
+                  type="button"
+                  className="btn btn-secondary btn-sm"
+                  onClick={() => {
+                     setTimer(timer + 5);
+                  }}
+               >
+                  +5
+               </button>
+               <button
+                  type="button"
+                  className="btn btn-secondary btn-sm"
+                  onClick={() => {
+                     setTimer(timer - 5);
+                  }}
+               >
+                  -5
+               </button>
+            </div>
+         </div>
+         <div className="display-1 text-center">{`${secondsToTime(Math.ceil(timer)).string}`}</div>
+         <button
+            type="button"
+            className="btn btn-danger btn-lg btn-block"
+            onClick={() => {
+               setStartFlag(true);
+            }}
+         >
+            Запуск
+         </button>
+      </div>
+   );
 };
 
 export default Timer;
